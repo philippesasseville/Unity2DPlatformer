41016e2e3aba7160f498d6f55f2eb7b09f880245
philippesasseville <philippe.sasseville@polymtl.ca>
2015-01-14 06:55:16 -0500

    unity project currently experimenting with Character controller2D by prim[31]

---
+++ 2DPlatformerTesting/Assets/scipts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private int idleState = Animator.StringToHash("Base Layer.idle");
+	private int runState = Animator.StringToHash("Base Layer.run");
+	private int jumpState = Animator.StringToHash("Base Layer.jump");
+
+	private CharacterController2D _controller;
+	private Animator _animator;
+
+	void Awake()
+	{
+		_controller = GetComponent<CharacterController2D>();
+	}
+}
